8a32d576d1470c66f7ea5941212e3a7dd2dd3799
antoine <antoine.stuff@pm.me>
2021-04-13 17:25:17 +0200

    index clean

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function getKit() {
   console.log("kit");
 }
 
-// Display kits
+// Display kits (index)
 function displayKits(kits) {
   let dateNow = new Date();
   let kitsActive = [];
@@ -47,23 +47,47 @@ function displayKits(kits) {
   // Sort active and inactive kits
   for (let kit of kits) {
     let lastUpdate = new Date(kit.updated_at);
-    let dateDifferenceDays =
-      (dateNow.getTime() - lastUpdate.getTime()) / (1000 * 3600 * 24);
-    dateDifferenceDays < 8 ? kitsActive.push(kit) : kitsInactive.push(kit);
+    let dateDifferenceMinutes = (dateNow.getTime() - lastUpdate.getTime()) / (1000 * 3600);
+    dateDifferenceMinutes < 30 ? kitsActive.push(kit) : kitsInactive.push(kit);
   }
-  for (let i = 0; i < kitsActive.length; i++) {
-    const elem = document.createElement("article");
-    const elemTitle = document.createElement("h2");
-    const elemUpdated = document.createElement("p");
-    elem.id = kitsActive[i].id;
-    elemTitle.innerHTML = kitsActive[i].name;
-    elemUpdated.innerHTML =
-      "last update: " +
-      new Date(kitsActive[i].updated_at).toLocaleString("en-GB");
-    document.getElementById("main").appendChild(elem);
-    elem.appendChild(elemTitle);
-    elem.appendChild(elemUpdated);
+  // Display title
+  const elemTitle = document.createElement("h1");
+  elemTitle.id = "subtitle";
+  elemTitle.innerHTML = "Smart Citizen Dashboard";
+  document.getElementById("main").appendChild(elemTitle);
+  // Display subtitle
+  const elemSubtitle = document.createElement("h2");
+  elemSubtitle.id = "subtitle";
+  elemSubtitle.innerHTML = `${kitsActive.length} Kits connected from a total of ${kitsActive.length + kitsInactive.length} (${Math.round((kitsActive.length / kitsInactive.length) * 100)} %)`;
+  elemTitle.parentNode.insertBefore(elemSubtitle, elemTitle.nextSibling);
+  let x = 0;
+  // Display active and inactive kits
+  while (x < 2) {
+    let currentKit;
+    x === 0 ? (currentKit = kitsActive) : (currentKit = kitsInactive);
+    for (let i = 0; i < currentKit.length; i++) {
+      const elem = document.createElement("article");
+      const elemTitle = document.createElement("h2");
+      const elemUpdated = document.createElement("p");
+      elem.id = currentKit[i].id;
+      elemTitle.innerHTML = currentKit[i].name;
+      elemUpdated.innerHTML = "last update: " + new Date(currentKit[i].updated_at).toLocaleString("en-GB");
+      document.getElementById("main").appendChild(elem);
+      document.getElementById("main").classList.remove("detail");
+      document.getElementById("main").classList.add("index");
+      elem.appendChild(elemTitle);
+      elem.appendChild(elemUpdated);
+      elem.onclick = function () {
+        displayKit(currentKit[i].id);
+      };
+    }
+    x++;
   }
+}
+
+// Display kit (detail)
+function displayKit(id) {
+  console.log(id);
 }
 
 // Get URL parameters
@@ -79,8 +103,7 @@ function alertUpdate(id, status) {
   let alert;
   switch (status) {
     case "tooManyRequests":
-      message =
-        "Too many requests, please wait 10 seconds before trying again.";
+      message = "Too many requests, please wait 10 seconds before trying again.";
       break;
     default:
       message = "";
