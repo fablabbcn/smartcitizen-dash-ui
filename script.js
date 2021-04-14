71344aac9bb29bc163d2d78598fd84fd3a8ac454
antoine <antoine.stuff@pm.me>
2021-04-14 17:46:44 +0200

    plotters

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function dashboardInit() {
   interface();
   loading(true);
   if (id) {
-    getKit();
+    getKit(id);
   } else if (tag) {
     getKits("tag", tag);
   } else if (city) {
@@ -41,8 +41,34 @@ function getKits(filterType = null, filterValue = null) {
 }
 
 // API get kit
-function getKit() {
-  console.log("kit");
+function getKit(id) {
+  const kitUrl = `https://api.smartcitizen.me/v0/devices/${id}`;
+  https: fetch(kitUrl)
+    .then((res) => {
+      if (res.status == 429) alertUpdate(id, "tooManyRequests");
+      return res.json();
+    })
+    .then((kit) => {
+      displayKit(kit);
+      getKitData(kit);
+    });
+}
+
+// Api get kit data
+function getKitData(kit) {
+  let d = new Date();
+  let today = d.toISOString().slice(0, 10);
+  let then = new Date(d.setDate(d.getDate()-5)).toISOString().slice(0, 10); // 5 days ago
+  for (let i = 0; kit.data.sensors.length > i; i++) {
+    const sensorUrl = `https://api.smartcitizen.me/v0/devices/${kit.id}/readings?sensor_id=${kit.data.sensors[i].id}&rollup=1h&from=${then}&to=${today}`;
+    https: fetch(sensorUrl)
+      .then((res) => {
+        return res.json();
+      })
+      .then((sensor) => {
+        displaySensor(kit, sensor, i);
+      });
+  }
 }
 
 // Display kits (index)
@@ -71,18 +97,20 @@ function displayKits(kits, filterType = null, filterValue = null) {
   // Sort active and inactive kits
   for (let kit of kitsFiltered) {
     let lastUpdate = new Date(kit.updated_at);
-    let dateDifferenceMinutes = (dateNow.getTime() - lastUpdate.getTime()) / (1000 * 3600);
-    dateDifferenceMinutes < 30 ? kitsActive.push(kit) : kitsInactive.push(kit);
+    let dateDifferenceMinutes = (dateNow.getTime() - lastUpdate.getTime()) / (1000 * 3600 * 24);
+    dateDifferenceMinutes < 1 ? kitsActive.push(kit) : kitsInactive.push(kit);
   }
   // Display title
   const elemTitle = document.createElement("h1");
   elemTitle.id = "title";
-  elemTitle.innerHTML = "Smart Citizen Dashboard";
+  let titleComplement;
+  filterType ? titleComplement = ": " + filterValue : titleComplement = "";
+  elemTitle.innerHTML = `Smart Citizen Dashboard${titleComplement}`;
   document.getElementById("main").appendChild(elemTitle);
   // Display subtitle
   const elemSubtitle = document.createElement("h2");
   elemSubtitle.id = "subtitle";
-  elemSubtitle.innerHTML = `${kitsActive.length} Kits connected from a total of ${kitsFiltered.length} (${Math.round((kitsActive.length / kitsFiltered.length) * 100)} %)`;
+  elemSubtitle.innerHTML = `${kitsActive.length} active kits today, of a total of ${kitsFiltered.length}`;
   elemTitle.parentNode.insertBefore(elemSubtitle, elemTitle.nextSibling);
   let x = 0;
   // Display active and inactive kits
@@ -134,13 +162,64 @@ function displayKits(kits, filterType = null, filterValue = null) {
 }
 
 // Display kit (detail)
-function displayKit(id) {
+function displayKit(kit) {
   document.getElementById("main").innerHTML = "";
-  // Display title
   const elemTitle = document.createElement("h1");
+  const elemLink = document.createElement("a");
+  const elemSensors = document.createElement("ul");
   elemTitle.id = "title";
-  elemTitle.innerHTML = "Smart Citizen Dashboard";
+  elemSensors.id = "sensors";
+  elemTitle.innerHTML = `Smart Citizen Dashboard: ${kit.name}`;
+  elemLink.innerHTML = 'More info on this kit';
+  elemLink.href = `https://smartcitizen.me/kits/${kit.id}`;
+  elemLink.target = "_blank";
   document.getElementById("main").appendChild(elemTitle);
+  document.getElementById("main").appendChild(elemLink);
+  document.getElementById("main").appendChild(elemSensors);
+}
+
+// Display sensor
+function displaySensor(kit, sensor, i) {
+  let readings = sensor.readings;
+  let data = [[], []];
+  for (const reading of readings) {
+    let date = new Date(reading[0]).getTime() / 1000;
+    data[0].push(date);
+    data[1].push(reading[1]);
+  }
+  if (data != undefined && data[0].length > 0) {
+    const elem = document.createElement("li");
+    const elemTitle = document.createElement("h1");
+    const elemValue = document.createElement("h2");
+    elemTitle.innerHTML = kit.data.sensors[i].description;
+    elemValue.innerHTML = kit.data.sensors[i].value + " " + kit.data.sensors[i].unit;
+    elem.id = kit.data.sensors[i].id;
+    elem.appendChild(elemTitle);
+    elem.appendChild(elemValue);
+    document.getElementById("sensors").appendChild(elem);
+    const canvasWidth = 600;
+    const canvasHeight = (canvasWidth / 3) * 2;
+    const opts = {
+      title: kit.data.sensors[i].description,
+      id: kit.data.sensors[i].id,
+      class: "chart",
+      width: canvasWidth,
+      height: canvasHeight,
+      series: [
+        {},
+        {
+          spanGaps: true,
+          label: sensor.sensor_key,
+          width: 1,
+          stroke: "rgba(0, 0, 0, 1",
+          fill: "rgba(0, 0, 0, 0.2)",
+          width: 1,
+        },
+      ]
+    };
+    let uplot = new uPlot(opts, data, document.getElementById(kit.data.sensors[i].id));
+  }
+  loading(false);
 }
 
 // Get URL parameters
