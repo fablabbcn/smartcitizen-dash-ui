5f124c48e87b6d7af7d174dc31b22a2687a988ee
antoine3000 <antoine.stuff@pm.me>
2021-08-17 19:48:42 +0200

    little cleaning session after trying to restructure everything

--- settings.js
+++ settings.js
@@ -1,37 +1,17 @@
-let settings = {
-  title: "Smart Citizen Dashboard",
-  logo: "logo-sc.svg",
-  filter: { search: true, type: "", value: "" },
-  // primarySensor: { id: 56, threshold: [20, 40] },
-  indexView: ["name", "city", "user", "tags", "id", "last_update"],
-  // https://developer.smartcitizen.me/#get-historical-readings
-  plots: {
-    rollup: '1m',
-    delta_days: 5,
-    labelsize: 11,
-    height_ratio: 1/3,
-    show_last_reading: false,
-  },
-  sensors: [
-    { id: 56, threshold: [20, 40] },
-    { id: 56, threshold: [40, 20] },
-    { id: 56, threshold: [20, 40] },
-    { id: 56, threshold: [40, 20] },
-  ],
-  styles: {
-    colorBase: "#000000",
-    colorBody: "#ffffff",
-    colorAction: "#0065ff",
-    colorBackground: "#dfdfdf",
-  },
-};
-
 // let settings = {
-//   title: "CO-MIDA Dashboard",
-//   logo: "logo-random.svg",
-//   filter: { search: false, type: "user", value: "IAAC-Cristian_Rizzuti" },
-//   primarySensor: { id: 56, threshold: [20, 40] },
-//   indexView: ["name","last_update"],
+//   title: "Smart Citizen Dashboard",
+//   logo: "logo-sc.svg",
+//   filter: { search: true, type: "", value: "" },
+//   // primarySensor: { id: 56, threshold: [20, 40] },
+//   indexView: ["name", "city", "user", "tags", "id", "last_update"],
+//   // https://developer.smartcitizen.me/#get-historical-readings
+//   plots: {
+//     rollup: '1m',
+//     delta_days: 5,
+//     labelsize: 11,
+//     height_ratio: 1/3,
+//     show_last_reading: false,
+//   },
 //   sensors: [
 //     { id: 56, threshold: [20, 40] },
 //     { id: 56, threshold: [40, 20] },
@@ -45,3 +25,33 @@ let settings = {
 //     colorBackground: "#dfdfdf",
 //   },
 // };
+
+let settings = {
+  title: "CO-MIDA Dashboard",
+  logo: "logo-random.svg",
+  filter: { search: false, type: "city", value: "Barcelona" },
+  primarySensor: { id: 56, threshold: [20, 40] },
+  // indexView: ["name","last_update"],
+  indexView: ["name", "city", "user", "tags", "id", "last_update"],
+  sensors: [
+    { id: 56, threshold: [20, 40] },
+    { id: 56, threshold: [40, 20] },
+    { id: 56, threshold: [20, 40] },
+    { id: 56, threshold: [40, 20] },
+  ],
+  plots: {
+    rollup: '1m',
+    delta_days: 5,
+    labelsize: 11,
+    height_ratio: 1 / 3,
+    show_last_reading: false,
+  },
+  styles: {
+    colorBase: "#000000",
+    colorBody: "#ffffff",
+    colorAction: "#0065ff",
+    colorBackground: "#dfdfdf",
+  },
+};
+
+// filter: { search: false, type: "user", value: "IAAC-Cristian_Rizzuti" },
