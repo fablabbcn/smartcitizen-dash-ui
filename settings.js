6c6b651547f634e507ee854a5db7e863becad4ad
antoine3000 <antoine.stuff@pm.me>
2021-08-20 17:26:22 +0200

    restore previous state before branching

--- settings.js
+++ settings.js
@@ -1,36 +1,30 @@
 let settings = {
-  filter: {type: "", value: "" },
-  primarySensor: { id: 56, threshold: [20, 40] },
-  indexView: ["name", "name", "name", "tags", "id", "last_update"],
+  title: "Smart Citizen Dashboard",
+  logo: "logo-sc.svg",
+  filter: { search: true, type: "", value: "" },
+  // primarySensor: { id: 56, threshold: [20, 40] },
+  indexView: ["name", "city", "user", "tags", "id", "last_update"],
+  // https://developer.smartcitizen.me/#get-historical-readings
+  plots: {
+    rollup: '1m',
+    delta_days: 5,
+    labelsize: 11,
+    height_ratio: 1/3,
+    show_last_reading: false,
+  },
+  sensors: [
+    { id: 56, threshold: [20, 40] },
+    { id: 56, threshold: [40, 20] },
+    { id: 56, threshold: [20, 40] },
+    { id: 56, threshold: [40, 20] },
+  ],
+  styles: {
+    colorBase: "#000000",
+    colorBody: "#ffffff",
+    colorAction: "#0065ff",
+    colorBackground: "#dfdfdf",
+  },
 };
-
-// let settings = {
-//   title: "Smart Citizen Dashboard",
-//   logo: "logo-sc.svg",
-//   filter: { search: true, type: "", value: "" },
-//   // primarySensor: { id: 56, threshold: [20, 40] },
-//   indexView: ["name", "city", "user", "tags", "id", "last_update"],
-//   // https://developer.smartcitizen.me/#get-historical-readings
-//   plots: {
-//     rollup: '1m',
-//     delta_days: 5,
-//     labelsize: 11,
-//     height_ratio: 1/3,
-//     show_last_reading: false,
-//   },
-//   sensors: [
-//     { id: 56, threshold: [20, 40] },
-//     { id: 56, threshold: [40, 20] },
-//     { id: 56, threshold: [20, 40] },
-//     { id: 56, threshold: [40, 20] },
-//   ],
-//   styles: {
-//     colorBase: "#000000",
-//     colorBody: "#ffffff",
-//     colorAction: "#0065ff",
-//     colorBackground: "#dfdfdf",
-//   },
-// };
 
 // let settings = {
 //   title: "CO-MIDA Dashboard",
